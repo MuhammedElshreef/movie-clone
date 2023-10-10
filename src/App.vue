3e5ea2346a84155e6b3f4b75bbfdba776a06185e
Muhammed Al-sayed <89754292+MuhammedElshreef@users.noreply.github.com>
2023-10-10 03:07:43 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import NavBar from './components/NavBar.vue'
-import { useStoreMovie } from './stores/storeMovie'
-const movies = useStoreMovie()
+// import { useStoreMovie } from './stores/storeMovie'
+// const movies = useStoreMovie()
 </script>
 <template>
   <div class="bg-[#141414]">
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@
     class="fixed bg-[#141414] min-h-screen w-20 border-r-[0.1px] border-gray-500 flex flex-col items-center pt-9 gap-16"
   >
     <button>
-      <img src="../assets/icons/homeIcon.svg" class="w-8 stroke-gray-700" alt="" />
+      <img src="../assets/icons/homeIcon.svg" class="w-8" alt="" />
     </button>
     <button>
       <img src="../assets/icons/movieIcon.svg" class="w-8" alt="" />
