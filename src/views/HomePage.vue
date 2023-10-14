393ba580e44b0a0239432c3027c3e4dff5c3f75c
Muhammed Al-sayed <89754292+MuhammedElshreef@users.noreply.github.com>
2023-10-14 02:04:10 +0200

    update

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref } from 'vue'
 import { useTrending } from '../stores/trending'
 import HeroBoard from '../components/HeroBoard.vue'
 import trendingMovie from '../components/trendingMovie.vue'
@@ -9,16 +8,6 @@ const trending = useTrending()
 </script>
 <template>
   <div class="lg:pl-[5rem] flex flex-col">
-    <div class="flex">
-      <div
-        v-show="!randomTrendingMovie"
-        class="h-[450px] w-full flex justify-center items-center bg-black"
-      >
-        <div
-          class="inline-block h-20 w-20 animate-spin rounded-full border-4 border-[#1E89DE] border-solid border-current border-r-transparent motion-reduce:animate-[spin_1.5s_linear_infinite]"
-        ></div>
-      </div>
-    </div>
     <div>
       <HeroBoard :show="trending.shows[0]" />
     </div>
